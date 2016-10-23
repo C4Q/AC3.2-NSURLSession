39ec8bd0633d58df84dcafae874b9d3b5f33b835
Marty Avedon <martyavedon@C4Qs-MacBook-Pro-72.local>
2016-10-23 17:58:43 -0400

    copied/pasted meat of the functions from catFactory and find/replaced by hand all instances of cat for dog.

--- InstaDogFactory.swift
+++ InstaDogFactory.swift
@@ -13,12 +13,43 @@ class InstaDogFactory {
     static let manager: InstaDogFactory = InstaDogFactory() 
     private init() {} //no way to initialize this outside
     
-    class func makeInstaDogs(apiEndpoint: String, callback: @escaping ([InstaDog]?) -> Void) {
+    //I realize that I was asked to make these functions with different names, but comparing them to the functions with the corresponding signatures in catFactory, it seems like some kind of mix-up.
     
+    internal func makeInstaDogs(from jsonData: Data) -> [InstaDog]? {
+        guard let instaDogsURL: URL = InstaDogFactory.manager.getResourceURL(from: fileName),
+            let instaDogData: Data = InstaDogFactory.manager.getData(from: instaDogsURL),
+            let instaDogsAll: [InstaCat] = InstaDogFactory.manager.getInstaDogs(from: instaDogData) else {
+                return nil
+        }
+        return [InstaDog]
     }
     
-    internal func getInstaDogs(from jsonData: Data) -> [InstaDog]? {
-        
-        return [InstaDog]()
+    class func getInstaDogs(apiEndpoint: String, callback: @escaping ([InstaDog]?) -> Void) {
+        if let validInstaDogEndpoint: URL = URL(string: apiEndpoint) {
+                
+            // 1. URLSession/Configuration
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+                
+            // 2. dataTaskWithURL
+            session.dataTask(with: validInstaDogEndpoint) { (data: Data?, response: URLResponse?, error: Error?) in
+                    
+            // 3. check for errors right away
+                if error != nil {
+                    print("Error encountered!: \(error!)")
+                }
+                    
+            // 4. printing out the data
+                if let validData: Data = data {
+                    print(validData)
+                        
+            // 5. reuse our code to make some cats from Data
+                    let allTheDogs: [InstaDog]? = InstaDogFactory.manager.getInstaDogs(from: validData)
+                    print("I'm super before")
+                    callback(allTheDogs)
+                }
+                }.resume()
+            print("I'm super after")
+        }
     }
 }
+
