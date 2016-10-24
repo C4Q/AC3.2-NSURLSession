8b5caec7b0df98d10781148590a8cd3bbba98388
Victor Zhong <viczhong@gmail.com>
2016-10-23 23:41:19 -0400

    Creating checkpoint

--- AC3.2-InstaCats-2/View Controllers/InstaCatTableViewController.swift
+++ AC3.2-InstaCats-2/View Controllers/InstaCatTableViewController.swift
@@ -9,41 +9,98 @@
 import UIKit
 
 class InstaCatTableViewController: UITableViewController {
-    internal let InstaCatTableViewCellIdentifier: String = "InstaCatCellIdentifier"
-    internal let instaCatJSONFileName: String = "InstaCats.json"
-    internal var instaCats: [InstaCat] = []
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let instaCatsAll: [InstaCat] = InstaCatFactory.makeInstaCats(fileName: instaCatJSONFileName) {
-            self.instaCats = instaCatsAll
-        }
-    }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.instaCats.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: InstaCatTableViewCellIdentifier, for: indexPath)
-        
-        cell.textLabel?.text = self.instaCats[indexPath.row].name
-        cell.detailTextLabel?.text = self.instaCats[indexPath.row].description
-        
-        return cell
-    }
-
+	internal let InstaCatTableViewCellIdentifier: String = "InstaCatCellIdentifier"
+	internal let instaCatJSONFileName: String = "InstaCats.json"
+	internal var instaCats: [InstaCat] = []
+	internal let instaCatEndpoint: String = "https://api.myjson.com/bins/254uw"
+	
+	func getInstaCats(apiEndpoint: String, callback: @escaping ([InstaCat]?) -> Void) {
+		if let validInstaCatEndpoint: URL = URL(string: apiEndpoint) {
+			
+			// 1. URLSession/Configuration
+			let session = URLSession(configuration: URLSessionConfiguration.default)
+			
+			// 2. dataTaskWithURL
+			session.dataTask(with: validInstaCatEndpoint) { (data: Data?, response: URLResponse?, error: Error?) in
+				
+				// 3. check for errors right away
+				if error != nil {
+					print("Error encountered!: \(error!)")
+				}
+				
+				// 4. printing out the data
+				if let validData: Data = data {
+					print(validData)
+					
+					// 5. reuse our code to make some cats from Data
+					
+					
+					// 6. if we're able to get non-nil [InstaCat], set our variable and reload the data
+					if let instaCats: [InstaCat] = InstaCatFactory.manager.getInstaCats(from: validData) {
+						//						self.instaCats = instaCats
+						// update the UI by wrapping the UI-updating code inside of a DispatchQueue closure
+						//						DispatchQueue.main.async {
+						//							self.tableView.reloadData()
+						//						}
+						callback(instaCats)
+						
+					}
+				}
+				}.resume()
+		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		//		if let instaCatsAll: [InstaCat] = InstaCatFactory.makeInstaCats(fileName: instaCatJSONFileName) {
+		//			self.instaCats = instaCatsAll
+		//			self.getInstaCats(from: instaCatEndpoint)
+		//		}
+		
+//		self.getInstaCats(apiEndpoint: instaCatEndpoint) { (instaCats: [InstaCat]?) in
+//			if instaCats != nil {
+//				for cat in instaCats! {
+//					print(cat.description)
+//					
+//					DispatchQueue.main.async {
+//						self.instaCats = instaCats!
+//						self.tableView.reloadData()
+//					}
+//				}
+//			}
+//		}
+		
+		self.getInstaCats(apiEndpoint: instaCatEndpoint) { (instaCats: [InstaCat]?) in
+			if let returnedInstaCats = instaCats {
+			self.instaCats = returnedInstaCats
+				DispatchQueue.main.async {
+					self.tableView.reloadData()
+				}
+			}
+		}
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	// MARK: - Table view data source
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.instaCats.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: InstaCatTableViewCellIdentifier, for: indexPath)
+		
+		cell.textLabel?.text = self.instaCats[indexPath.row].name
+		cell.detailTextLabel?.text = self.instaCats[indexPath.row].description
+		
+		return cell
+	}
 }
